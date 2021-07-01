1161683b675c7aaa9b2f8d25405f810960a3912e
nicholsondarius <darius.nicholson@digital.justice.gov.uk>
2021-07-01 14:30:08 +0100

    Remove Adam and Sam

--- settings.py
+++ settings.py
@@ -3,25 +3,29 @@ google_calendar_api = {
     "api_name": "calendar",
     "api_version": "v3",
     "scopes": ["https://www.googleapis.com/auth/calendar"],
-    "calendar_id": "qk7dfadvnmao3lgvb207pqd1bk@group.calendar.google.com",
+    "calendar_id": {
+        "dev": "qk7dfadvnmao3lgvb207pqd1bk@group.calendar.google.com",
+        "prod": "9c720gjf06r8odu2vhsfvd7e9k@group.calendar.google.com",
+    },
 }
 
 support_team = {
-    "g_sevens": ("George", "Adam", "Calum", "Karik", "Sam", "Jacob", "Kimberley"),
-    "everyone_else": (
-        "Darius",
+    "start_cycle_with": "everyone_else",
+    "g_sevens": ["George", "Calum", "Karik", "Jacob", "Kimberley"],
+    "everyone_else": [
         "Thomas",
         "Jake",
         "Tapan",
+        "Darius",
         "Anthony",
         "David",
         "Danjiv",
         "Lora",
         "Stephen",
-    ),
+    ],
 }
 
 date_range = {
-    "start_date": "2021-01-01",
-    "n_cycles": 15,
+    "start_date": "2021-07-01",
+    "n_cycles": 10,
 }
