21cde96d048f11f2dca9d6dc82937853c5427c7f
tamsinforbes <32667518+tamsinforbes@users.noreply.github.com>
2023-04-15 09:04:14 +0100

    test send message to channel

---
+++ .github/scripts/notify-channel.py
@@ -0,0 +1,33 @@
+import json
+import os
+
+import requests
+from requests.exceptions import HTTPError, RequestException, URLRequired
+from datetime import date
+
+# Get hook from GitHub secrets
+hook = os.getenv("TEST_GCAL_HOOK")
+# hook = os.getenv("DATA_ENG_HOOK")
+
+base_url = "https://hooks.slack.com/services/"
+
+
+# Get today's event details from calendar
+today = date.today()
+
+# Send message to slack channel
+def send_message(content, webhook):  # send message to webhook
+    message = {"text": content}
+    message_json = json.dumps(message)
+    return requests.post(webhook, message_json)
+
+text = f"Today's date is {today}"
+webhook_endpoint = f"{base_url}{hook}"
+
+send_message(text, webhook_endpoint)  
+
+# python .github/scripts/notify-channel.py
+
+# 1. connect to calendar and get calendar event details
+# 2. use webhook from github secrets to schedule messages
+
